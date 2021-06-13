1c7f91d204f325e20fe42d60239f750893beee7f
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-13 00:49:32 +0000

    Basic infrastructure

---
+++ .env/zsh.zsh
@@ -0,0 +1,11 @@
+load ~/.zsh/environments/helpers.zsh && save-function-list
+load ~/.zsh/environments/basic.zsh
+load ~/.zsh/environments/emacs.zsh
+
+start-emacs-session
+rename-first-tab
+
+# Custom functions & aliases.
+# ...
+
+report-custom-functions
