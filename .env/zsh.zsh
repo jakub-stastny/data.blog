d1e9619b0f302d672598938e62ca122a2e182a20
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2021-06-13 00:50:28 +0000

    .profile.zsh -> .env/zsh.zsh

--- .env/zsh.zsh
+++ .env/zsh.zsh
@@ -6,6 +6,11 @@ start-emacs-session
 rename-first-tab
 
 # Custom functions & aliases.
-# ...
+mkpost() {
+  test "$#" -eq 1 || (echo "Usage: mkpost hello-world" && return 1)
+  mkdir "posts/$(date +%y-%m-%d)-$1"
+  touch "posts/$(date +%y-%m-%d)-$1/$1.org"
+  echo "posts/$(date +%y-%m-%d)-$1/$1.org"
+}
 
 report-custom-functions
